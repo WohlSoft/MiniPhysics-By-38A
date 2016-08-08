46422bd21d27ef94132e13d2fb9966d9654adbb9
Wohlstand <admin@wohlnet.ru>
2016-08-09 01:34:39 +0300

    Renamed a folder for a packed archive

--- build.bat
+++ build.bat
@@ -3,7 +3,8 @@ call _paths.bat
 
 PATH=%QtDir%;%MinGW%;%GitDir%;%PATH%
 SET SEVENZIP=C:\Program Files\7-Zip
-IF NOT EXIST "%SEVENZIP%\7z.exe" SET SEVENZIP=C:\Program Files (x86)\7-Zip
+
+IF NOT EXIST "%SEVENZIP%\7z.exe" SET SEVENZIP=%ProgramFiles(x86)%\7-Zip
 IF NOT EXIST "%SEVENZIP%\7z.exe" SET SEVENZIP=%ProgramFiles%\7-Zip
 
 qmake MiniPhysics.pro CONFIG+=release CONFIG-=debug
@@ -12,13 +13,13 @@ IF ERRORLEVEL 1 goto error
 mingw32-make
 IF ERRORLEVEL 1 goto error
 
-md _packed
+md mini-physics
 cd bin
 windeployqt MiniPhysics.exe
 IF ERRORLEVEL 1 goto error
 cd ..
 
-"%SEVENZIP%\7z" a -tzip "_packed\mini-physics-demo-win32.zip" bin\
+"%SEVENZIP%\7z" a -tzip "mini-physics\mini-physics-demo-win32.zip" bin\
 
 goto quit
 :error
